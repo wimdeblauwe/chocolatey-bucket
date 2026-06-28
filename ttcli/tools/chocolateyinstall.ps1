4360e893f8abc2afc5db0c13b306a5900d392d1d
jreleaserbot <jreleaser@kordamp.org>
2026-06-28 16:21:35 +0000

    ttcli 1.12.4

--- ttcli/tools/chocolateyinstall.ps1
+++ ttcli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.24.0 at 2026-06-05T10:07:58.581538202Z
+# Generated with JReleaser 1.24.0 at 2026-06-28T16:21:17.989282224Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 
 Install-ChocolateyZipPackage `
     -PackageName 'ttcli' `
-    -Url 'https://github.com/wimdeblauwe/ttcli/releases/download/1.12.3/ttcli-1.12.3-windows-x86_64.zip' `
-    -Checksum '39256cc572d485bc54bbe680b6377bdb3c35fd36d230208f9de0d8695c5362bd' `
+    -Url 'https://github.com/wimdeblauwe/ttcli/releases/download/1.12.4/ttcli-1.12.4-windows-x86_64.zip' `
+    -Checksum '7467c5d34fcb74c3c8113388dc4315be6718e50ade454cbd9b504913be08f420' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
